4b5a0a50e574a25df2771d0f3600e87930487f67
Ahmad Hasyir <hasyir87@gmail.com>
2025-11-07 15:14:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5512298805-6439c",
+  "appId": "1:729960238897:web:78258f520b07022977aa10",
+  "apiKey": "AIzaSyDyvCJ0BA7BHBMJdnIoKAgk4ePI6wMACPM",
+  "authDomain": "studio-5512298805-6439c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "729960238897"
+};
