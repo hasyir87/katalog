79e3e32a2411434cccc30ade53738da380147f12
Ahmad Hasyir <hasyir87@gmail.com>
2025-11-07 15:27:47 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-5512298805-6439c",
-  "appId": "1:729960238897:web:78258f520b07022977aa10",
-  "apiKey": "AIzaSyDyvCJ0BA7BHBMJdnIoKAgk4ePI6wMACPM",
-  "authDomain": "studio-5512298805-6439c.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "729960238897"
+  "projectId": "katalog-parfum-next",
+  "appId": "1:782806346931:web:5337213437255858971165",
+  "apiKey": "AIzaSyAz_jC69yq_8Kx04zYjW1zJ5k-FpCk1i3w",
+  "authDomain": "katalog-parfum-next.firebaseapp.com",
+  "storageBucket": "katalog-parfum-next.appspot.com",
+  "messagingSenderId": "782806346931",
+  "measurementId": "G-L9E1V3E28V"
 };
