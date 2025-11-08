a10cbfd0d6962b9b8f402762b17aa769fe2a46b4
Ahmad Hasyir <hasyir87@gmail.com>
2025-11-08 09:41:00 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/api

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,17 +3,7 @@
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'
-
-// Hardcoded the correct config to ensure it's always used.
-const firebaseConfig = {
-  "projectId": "katalog-parfum-next",
-  "appId": "1:782806346931:web:5337213437255858971165",
-  "apiKey": "AIzaSyAz_jC69yq_8Kx04zYjW1zJ5k-FpCk1i3w",
-  "authDomain": "katalog-parfum-next.firebaseapp.com",
-  "storageBucket": "katalog-parfum-next.appspot.com",
-  "messagingSenderId": "782806346931",
-  "measurementId": "G-L9E1V3E28V"
-};
+import { firebaseConfig } from './config';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
