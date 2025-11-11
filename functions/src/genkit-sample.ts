cc56f0e33b5a4e8d5d92795db6d5ad8d6a935238
Ahmad Hasyir <hasyir87@gmail.com>
2025-11-11 13:36:16 +0000

    Try fixing this error: `Console Error: Failed to fetch from https://gene

--- functions/src/genkit-sample.ts
+++ functions/src/genkit-sample.ts
@@ -1,16 +1,16 @@
 // Import the Genkit core libraries and plugins.
 import {genkit, z} from "genkit";
-import {googleAI} from "@genkit-ai/googleai";
+import {googleAI} from "@genkit-ai/google-genai";
 
 // Import models from the Google AI plugin. The Google AI API provides access to
-// several generative models. Here, we import Gemini 2.0 Flash.
-import {gemini20Flash} from "@genkit-ai/googleai";
+// several generative models. Here, we import Gemini Pro.
+import {geminiPro} from "@genkit-ai/google-genai";
 
 // Cloud Functions for Firebase supports Genkit natively. The onCallGenkit function creates a callable
 // function from a Genkit action. It automatically implements streaming if your flow does.
 // The https library also has other utility methods such as hasClaim, which verifies that
 // a caller's token has a specific claim (optionally matching a specific value)
-import { onCallGenkit, hasClaim } from "firebase-functions/https";
+import { onCallGenkit, hasClaim } from "firebase-functions/v2/https";
 
 // Genkit models generally depend on an API key. APIs should be stored in Cloud Secret Manager so that
 // access to these sensitive values can be controlled. defineSecret does this for you automatically.
@@ -44,7 +44,7 @@ const menuSuggestionFlow = ai.defineFlow({
     const prompt =
       `Suggest an item for the menu of a ${subject} themed restaurant`;
     const { response, stream } = ai.generateStream({
-      model: gemini20Flash,
+      model: geminiPro,
       prompt: prompt,
       config: {
         temperature: 1,
