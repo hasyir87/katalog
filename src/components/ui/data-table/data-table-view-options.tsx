68a49bc8ff03970c59e68a0d88e18be5beea7419
Ahmad Hasyir <hasyir87@gmail.com>
2025-11-14 10:16:52 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@r

--- src/components/ui/data-table/data-table-view-options.tsx
+++ src/components/ui/data-table/data-table-view-options.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
-import { MixerHorizontalIcon } from "@radix-ui/react-icons"
+import { SlidersHorizontal } from "lucide-react"
 import { type Table } from "@tanstack/react-table"
 
 import { Button } from "@/components/ui/button"
@@ -16,7 +16,7 @@ export function DataTableViewOptions<TData>({ table }: DataTableViewOptionsProps
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="sm" className="ml-auto hidden h-8 lg:flex">
-          <MixerHorizontalIcon className="mr-2 h-4 w-4" />
+          <SlidersHorizontal className="mr-2 h-4 w-4" />
           View
         </Button>
       </DropdownMenuTrigger>
